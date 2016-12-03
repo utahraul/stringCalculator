1d9edbc05a743b9011707e8ca1df17beb960f50a
utahraul <raul.hernandez@testeandosoftware.com>
2016-12-03 01:05:29 +0100

    Test 1 added

---
+++ src/stringCalculator.js
@@ -0,0 +1,8 @@
+var stringCalculator = function(string){
+  console.log(string.length)
+  if (string.length = 0){
+    return(0);
+  }
+};
+
+module.exports = stringCalculator;
---
+++ test/stringCalculator-test.js
@@ -0,0 +1,12 @@
+//test/stringCalculator-test.js
+var chai = require('chai');
+var expect = chai.expect; // we are using the "expect" style of Chai
+var stringCalculator = require('./../src/stringCalculator.js');
+
+describe('stringCalculator', function() {
+  it('stringCalculator() should return 0 if an empty string is passed in', function() {
+    var emptyStringCalculator = stringCalculator('');
+    expect(emptyStringCalculator).to.equal(0);
+  });
+
+});
